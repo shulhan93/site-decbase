3655aff16c86d064473dfaf11b6260621045decc
shulgan-andrei <eng.shulhan.andrei@gmail.com>
2024-08-02 11:30:59 +0300

    add slider in service section

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 import "../scss/main.scss";
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+
+const swiper = new Swiper(".service__slider", {
+  modules: [Navigation],
+  slidesPerView: 3,
+  spaceBetween: 37,
+  navigation: {
+    nextEl: ".slider-btn-next",
+    prevEl: ".slider-btn-prev",
+  },
+});
